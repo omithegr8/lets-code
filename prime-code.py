7de9cba758f492f9160a86f0e9515247e844a33b
omithegr8 <omikushwaha80@gmail.com>
2020-11-05 15:58:44 +0530

    prime-identifying-code

---
+++ prime-code.py
@@ -0,0 +1,10 @@
+num = int(input("enter a number : "))
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("it is not a prime number")
+            break
+    else:
+        print("it is a prime number")
+
